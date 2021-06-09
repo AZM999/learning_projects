9183f5ee06186da51e183d640e93fe0358bebddc
azmat <77288549+AZM999@users.noreply.github.com>
2021-06-09 16:56:45 +0530

    ISS_location_plotter
    iss_locater that plots the location of iss with timestamp in a map using libraries in python

---
+++ iss_locator.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import plotly.express as px
+
+url = 'http://api.open-notify.org/iss-now.json'
+
+df = pd.read_json(url)
+print(df)
+
+df['latitude'] = df.loc['latitude','iss_position']
+df['longitude'] = df.loc['longitude','iss_position']
+
+df.reset_index(inplace = True)
+
+
+df = df.drop(['index','message'], axis=1)
+
+fig = px.scatter_geo(df,lat='latitude',lon='longitude')
+
+print(df)
+fig.show()
